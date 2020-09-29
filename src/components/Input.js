bcba8a61d96891b1c7357a80e7ca04cd0937c654
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-29 10:10:33 +0300

    Input component added

---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Input() {
+    return (
+        <div>
+            <label for="todo">To Do:</label>
+            <input type="text" id="todo" name="todo" />
+        </div>
+    )
+}
