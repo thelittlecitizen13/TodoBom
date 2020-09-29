02c7c66625492330301628e217ea0032221b5fbf
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-29 12:14:13 +0300

    adding the input to list

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,22 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Input(props) {
-    // function handleSubmit(e)
-    // {
-        
-    //     props.handleClick();
-
-    // }
-    // function handleChange(e)
-    // {
-    //     props.setToDo(e.target.value)
-    // }
+    const [state, setstate] = useState("");
     return (
         <div>
             <form>
                 <label for="todo">To Do:</label>
-                <input type="text" id="todo" name="todo" />
-                <button type="button" value="Submit" onClick={() => props.handleClick("h")}/>
+                <input type="text" id="todo" name="todo" onInput={(val) => setstate(val.target.value)}/>
+                <button type="button" value="Submit" onClick={() => props.handleClick(state)}/>
             </form>
             
         </div>
